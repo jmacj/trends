730dd6f3e0300d3a11ed64ab4cddea396f6e6025
jmacj <jm.nz0723@gmail.com>
2018-05-28 02:40:03 +0800

    fixed db config

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ DEBUG=True
 SECRET_KEY="THIS IS SECRET"
 
 # DB CONFIG
-DB_NAME="trends"
+DB_NAME=os.getenv("DV_NAME") or "trends"
 DB_USER=os.getenv("DB_UNAME") or "root"
 DB_PASSWORD=os.getenv("DB_PASS") or ""
 DB_HOST=os.getenv("DB_HOST") or "localhost"
